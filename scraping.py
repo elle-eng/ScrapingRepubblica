aff7727dd0249ee23bf6ed29559de0680b9c67be
elle-eng <elena.zanet@uniupo.it>
2025-07-03 20:11:54 +0200

    feat: add data to xml files

--- scraping.py
+++ scraping.py
@@ -1,5 +1,63 @@
-from playwright.sync_api import sync_playwright
-import time 
+from playwright.sync_api import sync_playwright, Error
+from xml_utils import save_article_in_xml
+
+def scraping_articolo_digitale(page):
+    article = {
+        "type": "digitale"
+    }
+
+    if page.query_selector('h1.story__title'):
+        article["title"] = page.locator('h1.story__title').text_content()
+    else:
+        article["title"] = ""
+
+    if page.query_selector('.story__summary p'):
+        article["subtitle"] = page.locator('.story__summary p').first.text_content()
+    else:
+        article["subtitle"] = ""
+
+    if page.query_selector('em.story__author'):
+        article["author"] = page.locator('em.story__author').first.text_content()
+    else:
+        article["author"] = ""
+
+    if page.query_selector('div time.story__date'):
+        article["date"] = " ".join(page.locator('div time.story__date').text_content().split(' ')[:3])
+    else:
+        article["date"] = ""
+
+    if page.query_selector('div.story__text'):
+        article["content"] = page.locator('div.story__text').first.text_content()
+    else:
+        article["content"] = ""
+
+    save_article_in_xml(article)
+
+def scraping_articolo_cartaceo(page):
+    article = {
+        "type": "cartaceo"
+    }
+
+    article["title"] = page.locator('h1').first.text_content()
+    article["subtitle"] = ""
+    article["author"] = ""
+
+    if page.query_selector('article .edizione'):
+        article["date"] = page.locator('article .edizione').text_content().split(',')[0]
+    else:
+        article["date"] = ""
+
+    paragraphs = []
+    for p in page.query_selector_all('article p'):
+        paragraph = p.text_content().strip()
+        if paragraph:
+            paragraphs.append(paragraph)
+       
+    print('paragraphs',paragraphs)
+
+    article["content"] = "\n\n".join(paragraphs)
+
+    save_article_in_xml(article)
 
 link_ricerca = "https://ricerca.repubblica.it/ricerca/repubblica?query=climatico+climatica&fromdate=2020-01-01&todate=2025-06-29&sortby=score&author=&mode=any"
 email_google = 'elena.zanet@uniupo.it'
@@ -40,12 +98,26 @@ with sync_playwright() as p:
         page.wait_for_selector('section#lista-risultati article h1 a')
         links = page.locator('section#lista-risultati article h1 a')
         hrefs = links.evaluate_all("links => links.map(link => link.href)")
-        print('page:',page_num,'- hrefs:',len(hrefs))
-        
+        # print('page:',page_num,'- hrefs:',len(hrefs))
+
         for href in hrefs:
-            page.goto(href)
-            
-            page.go_back()
+            try:
+                page.goto(href)
+                isDigitale = page.locator('main article.story').count() > 0
+                isCartaceo = page.locator('body.premium-template').count() > 0
+                
+                if isDigitale:
+                    scraping_articolo_digitale(page)
+                    print('articolo digitale')
+                elif isCartaceo:
+                    scraping_articolo_cartaceo(page)
+                    print('articolo cartaceo')
+                else:
+                    print('altro tipo di articolo')
+            except Error as e:
+                print("Errore di playwright su ", href, "-", e)
+            finally:
+                page.go_back()
 
     while True:
         pass
---
+++ xml_utils.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+
+i = 1
+
+def save_article_in_xml(article):
+    global i 
+
+    root = ET.Element("article")
+    print('article', article)
+    ET.SubElement(root, "type").text = article["type"]
+    ET.SubElement(root, "title").text = article["title"]
+    ET.SubElement(root, "subtitle").text = article["subtitle"]
+    ET.SubElement(root, "author").text = article["author"]
+    ET.SubElement(root, "date").text = article["date"]
+    ET.SubElement(root, "content").text = article["content"]
+
+
+    filename = f"articles/rp_{i}.xml"
+    i = i+1
+
+    tree = ET.ElementTree(root)
+
+    with open(filename, "wb") as f:
+        tree.write(f, encoding="utf-8", xml_declaration=True)
+
+     
