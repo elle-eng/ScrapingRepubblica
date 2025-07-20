0244352806daeca00955272b6e41b2b3c7c1e627
elle-eng <elena.zanet@uniupo.it>
2025-07-20 19:06:07 +0200

    feat

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -12,3 +12,17 @@ l2 = list(l)
 l2.append(1000)
 print('l2', l2) #[34,2,6,3,1000]
 print('l',l) #[34,2,6,3]
+
+#list comprehension
+
+#creare una lista con gli elementi doppi di l
+l_doppi = [x*2 for x in l]
+print(l_doppi)
+
+#creare una lista con gli elementi di l come stringhe
+l_str = [str(numero) for numero in l]
+print(l_str)
+
+#creare una lista con gli elementi pari divisi per 2
+l_filtrata_mappata = [numero//2 for numero in l if numero % 2 == 0]
+print(l_filtrata_mappata)
