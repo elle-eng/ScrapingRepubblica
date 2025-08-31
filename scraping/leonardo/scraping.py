33e940d0dae1e50ff2c0ba4fe1b60d8942861928
elle-eng <elena.zanet@uniupo.it>
2025-08-31 19:06:36 +0200

    feat

--- scraping/leonardo/scraping.py
+++ scraping/leonardo/scraping.py
@@ -6,13 +6,13 @@ leonardo_news_selectors = {
     "date":"div.hero-slide--content--description",
     "content": "div.check-html-content"
 }
-# scrap_section(
-#     "news"
-#     "div.news-stories-card",
-#     leonardo_news_selectors,
-#     "scraping/leonardo/news",
-#     "https://www.leonardo.com/it/media-hub/news-stories?_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_formDate=1756653175484&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateTo=31.08.2025&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterTypeCategories=16624027%2C16624028&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateFrom=01.01.2020&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_dataFilterTypeReq=Year"
-# )
+scrap_section(
+    "news",
+    "div.news-stories-card",
+    leonardo_news_selectors,
+    "scraping/leonardo/news",
+    "https://www.leonardo.com/it/media-hub/news-stories?_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_formDate=1756653175484&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateTo=31.08.2025&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterTypeCategories=16624027%2C16624028&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateFrom=01.01.2020&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_dataFilterTypeReq=Year"
+)
 
 #STAMPA
 leonardo_stampa_selectors = {
@@ -20,10 +20,10 @@ leonardo_stampa_selectors = {
     "date":"p.content-secondary",
     "content": "div.check-html-content",
 }
-scrap_section(
-    "stampa",
-    "h3.press-release-card--content--title",
-    leonardo_stampa_selectors,
-    "scraping/leonardo/stampa",
-    "https://www.leonardo.com/it/media-hub/press-releases?_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_page=2&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateFrom=01.01.2020&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateTo=31.08.2025&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_dataFilterTypeReq=Year"
-)
+# scrap_section(
+#     "stampa",
+#     "h3.press-release-card--content--title",
+#     leonardo_stampa_selectors,
+#     "scraping/leonardo/stampa",
+#     "https://www.leonardo.com/it/media-hub/press-releases?_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_page=2&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateFrom=01.01.2020&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_filterDateTo=31.08.2025&_com_leonardocompany_list_content_viewer_portlet_ListContentViewerPortlet_dataFilterTypeReq=Year"
+# )
